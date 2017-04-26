0be5d5444b0d5e68d4c96aa6d592ce22fe32eb09
takuzoo3868 <sata3868.k.serpentis@gmail.com>
2017-04-26 23:29:36 +0900

    :sparkles: add chapter00 exercises

---
+++ chapter00/exercises04.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout <<
+    "// a small C++ program\n"
+    "#include <iostream>\n"
+    "\n"
+    "int main()\n"
+    "{\n"
+    "\tstd::cout << \"Hello, world!\" << std::endl;\n"
+    "\treturn 0;\n"
+    "}" 
+  << endl;
+  
+  return 0;
+}
